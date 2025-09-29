9d86f89e2cf4743d2c58ece037a5867d558e7564
Ridwan Malik <34468231+ridwanmalik@users.noreply.github.com>
2025-09-29 21:39:27 +0600

    feat: enhance About and Home page layouts and content structure

--- components/pages/Home.tsx
+++ components/pages/Home.tsx
@@ -1,21 +1,25 @@
 import Header from "@/components/shared/Header"
-import Link from "next/link"
+import { ridwan } from "@/lib/assets"
 import { PERSONAL_INFO } from "@/lib/constants"
+import Image from "next/image"
+import Link from "next/link"
 
 const Home = () => {
   return (
-    <section id="home" className="min-h-screen w-full flex flex-col">
+    <section id="home" className="min-h-screen w-full flex flex-col bg-gradient-to-t from-[#020b16]/50 to-transparent">
       <Header className="w-full" />
-      <div className="flex-1 flex justify-center items-center">
-        <div className="container mx-auto px-4 flex flex-col justify-center h-full max-w-4xl">
-          <div className="space-y-4">
+      <div className="flex-1 flex flex-col">
+        <div className="container mx-auto px-4 flex-1 flex flex-col lg:flex-row justify-center items-center h-full gap-8">
+          <div className="flex-1 space-y-4">
             <p className="text-custom-accent font-mono text-lg">{PERSONAL_INFO.intro}</p>
-            <h1 className="text-5xl md:text-7xl font-bold text-custom-foreground leading-tight">{PERSONAL_INFO.name}</h1>
-            <h2 className="text-4xl md:text-6xl font-bold text-custom-secondary leading-tight">{PERSONAL_INFO.title}</h2>
+            <h1 className="text-5xl md:text-7xl font-bold text-custom-foreground leading-tight">
+              {PERSONAL_INFO.name}
+            </h1>
+            <h2 className="text-4xl md:text-6xl font-bold text-custom-secondary leading-tight">
+              {PERSONAL_INFO.title}
+            </h2>
             <div className="max-w-2xl">
-              <p className="text-custom-secondary leading-relaxed">
-                {PERSONAL_INFO.description}
-              </p>
+              <p className="text-custom-secondary leading-relaxed">{PERSONAL_INFO.description}</p>
             </div>
             <div className="pt-8">
               <Link
@@ -24,6 +28,9 @@ const Home = () => {
                 Check out my work!
               </Link>
             </div>
+          </div>
+          <div className="flex justify-end items-end self-end">
+            <Image src={ridwan} alt="Ridwan Malik" width={455} height={1106} className="h-full" />
           </div>
         </div>
       </div>
